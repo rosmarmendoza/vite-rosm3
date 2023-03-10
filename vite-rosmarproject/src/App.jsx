d16380d85b33461dfc84b8785430c2bdcb38ccde
Rosmar Mendoza <mendozarosmar@gmail.com>
2023-03-09 21:53:24 -0500

    comiit form 3

--- vite-rosmarproject/src/App.jsx
+++ vite-rosmarproject/src/App.jsx
@@ -16,6 +16,16 @@ export const App = () => {
     setColor(event.target.value);
   }
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (name.trim().length < 3 || /^\s/.test(name) || color.trim().length < 6) {
+      setError(true);
+    } else {
+      setError(false);
+    }
+    setFormSubmitted(true);
+  };
+
   return (
     <div className="App">
       <h1>Elige un color</h1>
