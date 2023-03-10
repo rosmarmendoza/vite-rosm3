3e3d3e8cd0c2cce85bfa2c0234aa7e9a1443ce6b
Rosmar Mendoza <mendozarosmar@gmail.com>
2023-03-09 22:12:18 -0500

    hexadecimal prop

--- vite-rosmarproject/src/Components/Card/Card.jsx
+++ vite-rosmarproject/src/Components/Card/Card.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function Card({ name }) {
+function Card({ name, colorHex }) {
   return (
     <div style={{ width: "300px", border: "1px solid gray", textAlign: "center" }}>
       <h2>Hola {name}!</h2>
